cb160ea1a17ba9fe457584ad94037731abafc1b8
Anthony Dees <spirawebdev@gmail.com>
2020-10-16 14:36:35 -0700

    Working parallax header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Parallax } from "react-parallax"
 import Navigation from "./Navigation";
 import Header from "./Header";
+import About from "./About"
 
 function App() {
   return (
     <div>
-    <Navigation />,
+    <Navigation />
     <Header />
+    <About />
     </div>
   );
 }
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -1,12 +1,29 @@
 import React from "react"
+import { Parallax } from "react-parallax"
+
+const image1 = "https://images.unsplash.com/photo-1554118811-1e0d58224f24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2660&q=80"
+
+const inlineStyle = {
+    background: "#fff",
+    left: "40%",
+    top: "40%",
+    position: "absolute",
+    padding: "20px",
+    transform: "translate",
+    font: "roboto",
+
+}
 
 function Header() {
     return(
         <div className="parallax">
-            <h1 className="title">Hey</h1>
-           <img src={require("./parallax-photo.jpg")} alt="Background"></img>
+            <Parallax bgImage={image1}>
+                <div style={{height:500}}>
+                    <div style={inlineStyle}>HTML Inside the parallax</div>
+                </div>
+            </Parallax>
         </div>
-    )
+    );
 }
 
 
